9ff11e845c84f9f6cf517b01e69fab97f18d62ca
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-21 13:51:04 -0300

    Add project files.

---
+++ Manipulando_Aquivos/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulando_Aquivos
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public char Gender { get; set; }
+
+        public Person(string n, char g)
+        {
+            this.Name = n;
+            this.Gender = g;
+        }
+
+        public override string ToString()
+        {
+            return ("Nome: "+this.Name+"\nGenero: "+Gender+"\n");
+        }
+    }
+}
---
+++ Manipulando_Aquivos/Program.cs
@@ -0,0 +1,82 @@
+﻿using Manipulando_Aquivos;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string file;
+
+
+        Person person = CreatePerson();
+        Person person2 = CreatePerson();
+
+        WriteFile(person);
+        WriteFile(person2);
+
+        Console.Clear();
+
+        Console.WriteLine("Informe o nome do arquivo: ");
+        file= Console.ReadLine();
+
+        var texto = ReadFile(file);
+
+        Console.WriteLine(texto);
+
+
+
+
+
+
+
+
+        Person CreatePerson()
+        {
+            Console.Write("informe o nome da pessoa: ");
+            var name = Console.ReadLine();
+
+            Console.Write("\ninforme o genero da pessoa: ");
+            var gender = char.Parse(Console.ReadLine());
+
+
+            return  new Person(name, gender);
+        }
+
+        void WriteFile(Person person)
+        {
+            try
+            {
+                var texto = ReadFile("backup.txt");
+                StreamWriter sw = new StreamWriter("backup.txt");
+                sw.WriteLine(texto+person.ToString());
+                sw.Close();
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Registro gravado com sucesso!!");
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        string ReadFile(string file)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(file);
+                var text=sr.ReadToEnd();
+                sr.Close();
+                return text;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("arquivo nao encontrado");
+            }
+            return "";
+        }
+    }
+}
